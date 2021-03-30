ddfee47502e5aa7f2f6f67180471d93a02acb747
SavageBlink <31193280+SavageBlink@users.noreply.github.com>
2021-03-30 15:28:55 +0200

    adding few basic things

--- src/com/company/Horse.java
+++ src/com/company/Horse.java
@@ -1,4 +1,45 @@
 package com.company;
 
 public class Horse {
+
+    Color color;
+    int relativePosition; // from -1 to 51,52-55, -1 : home, 52-55 : arrow
+    int id;
+    boolean safe;
+
+    public Horse(Color color,int relativePosition,int id){
+        this.color = color;
+        this.relativePosition = relativePosition;
+        this.safe = false;
+        this.id = id;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getRelativePosition() {
+        return relativePosition;
+    }
+
+    public boolean isSafe() {
+        return safe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setColor(Color color){
+        this.color = color;
+    }
+
+    public void setRelativePosition(int relativePosition){
+        this.relativePosition = relativePosition;
+    }
+
+    public void flipSafe(){
+        this.safe = !safe;
+    }
+
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,48 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Player {
+    Color color;
+    ArrayList<Horse> pawns = new ArrayList<Horse>();
+    int homeCase;
+
+    public Player(Color color){
+        this.color = color;
+        for(int i = 0;i < 4;i++){
+            Horse current = new Horse(this.color,-1,i);
+            pawns.add(current);
+        }
+
+        switch (this.color){
+            case RED -> this.homeCase = 0;
+            case BLUE -> this.homeCase = 13;
+            case YELLOW -> this.homeCase = 26;
+            case GREEN ->  this.homeCase = 39;
+        }
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public ArrayList<Horse> getPawns() {
+        return pawns;
+    }
+
+    public int getHomeCase() {
+        return homeCase;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setHomeCase(int homeCase) {
+        this.homeCase = homeCase;
+    }
+
+    private void setPawns(ArrayList<Horse> pawns) {
+        this.pawns = pawns;
+    }
 }
